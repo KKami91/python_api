27be638e9bd32ddfe9c196b21225f3c988beb43d
KKami91 <ghkdth919@gmail.com>
2024-07-04 16:21:29 +0900

    build1

---
+++ main.py
@@ -0,0 +1,156 @@
+from fastapi import FastAPI, HTTPException, BackgroundTasks
+import os
+from pydantic import BaseModel
+import boto3
+import pandas as pd
+from prophet import Prophet
+import datetime
+from botocore.exceptions import ClientError
+from datetime import datetime
+from dateutil import relativedelta
+from datetime import timedelta
+from typing import Optional
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+app = FastAPI()
+load_dotenv()
+
+
+# AWS
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+TABLE_NAME = 'MyMentos'
+
+dynamodb = boto3.client('dynamodb', 
+                        aws_access_key_id=AWS_ACCESS_KEY,
+                        aws_secret_access_key=AWS_SECRET_KEY,
+                        region_name=AWS_REGION)
+
+
+
+MONGODB_URI = os.getenv('MONGODB_URI')
+client = MongoClient(MONGODB_URI)
+db = client.get_database("heart_rate_db")
+prediction_collection = db.prediction_results
+
+
+# 시간을 한국 시간으로 변형
+def conv_ds(startTime):
+    startTime = startTime.replace('T', ' ')
+    startTime = startTime.replace('Z', '')
+    return add_9(startTime)
+
+def add_9(startTime):
+    Year = startTime[:4]
+    Month = startTime[5:7]
+    Day = startTime[8:10]
+    Hour = startTime[11:13]
+    Min = startTime[14:16]
+    Sec = startTime[17:19]
+    if (int(Hour) + 9) >= 24 :
+        if get_month_lastday(int(Year), int(Month)) == int(Day):
+            if int(Month) == 12:
+                Year = str(int(Year) + 1)
+                Month = '01'
+                Day = '01'
+                Hour = str(int(Hour) + 9 - 24).zfill(2)
+            else:
+                Month = str(int(Month) + 1).zfill(2)
+                Day = '01'
+                Hour = str(int(Hour) + 9 - 24).zfill(2)
+        else:
+            Day = str(int(Day) + 1).zfill(2)
+            Hour = str(int(Hour) + 9 - 24).zfill(2)
+    else:
+        Hour = str(int(Hour) + 9).zfill(2)     
+    return Year + '-' + Month + '-' + Day + ' ' + Hour + ':' + Min + ':' + Sec
+        
+def get_month_lastday(year, month):
+    next_month = datetime(year=year, month=month, day=1).date() + relativedelta.relativedelta(months=1)
+    return (next_month - timedelta(days=1)).day
+
+def query_heart_rate_data(user_email: str, limit: int = 40000):
+    items = []
+    last_evaluated_key = None
+    try:
+        while len(items) < limit:
+            query_params = {
+                'TableName': TABLE_NAME,
+                'KeyConditionExpression': 'PK = :pk begins_with(SK, :sk_prefix)',
+                'ExpressionAttributeValues': {
+                    ':pk': {'S': f'U#{user_email}'},
+                    ':sk_prefix': {'S': f'HeartRateRecord#'},
+                },
+                'Limit': min(limit - len(items), 1000)
+            }
+            if last_evaluated_key:
+                query_params['ExclusiveStartKey'] = last_evaluated_key
+            
+            response = dynamodb.query(**query_params)
+            
+            items.extend(response['Items'])
+            
+            last_evaluated_key = response.get('LastEvaluatedKey')
+            if not last_evaluated_key or len(items) >= limit:
+                break
+        return items[-limit:]
+    except ClientError as e:
+        print(f"An error occurred: {e.response['Error']['Message']}")
+        return None
+
+def process_heart_rate_data(items):
+    processed_data = []
+    for item in items:
+        sk_parts = item['SK']['S'].split('#')
+        if len(sk_parts) >= 2 and sk_parts[0] == 'HeartRateRecord':
+            timestamp = sk_parts[1]
+            converted_timestamp = conv_ds(timestamp)
+            processed_data.append({
+                'ds': converted_timestamp,
+                'y': int(item['recordInfo']['M']['samples']['L'][0]['M']['beatsPerMinute']['N'])
+            })
+    return processed_data
+
+def save_prediction_to_mongodb(user_email: str, prediction_data):
+    prediction_collection.insert_one({
+        "user_email": user_email,
+        "prediction_date": datetime.now(),
+        "data": prediction_data.to_dict('records')
+    })
+
+def predict_heart_rate(df):
+    model = Prophet(changepoint_prior_scale=0.001,
+                    changepoint_range=0.95,
+                    daily_seasonality=True,
+                    weekly_seasonality=False,
+                    yearly_seasonality=False,
+                    interval_width=0.95)
+    model.fit(df)
+    
+    future = model.make_future_dataframe(periods=60*24*3, freq='min')
+    forecast = model.predict(future)   
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+@app.post("/analyze_and_predict")
+async def analyze_and_predict(user_email: str):
+    items = query_heart_rate_data(user_email)
+    if not items:
+        raise HTTPException(status_code=404, detail="유저 정보 없음")
+    
+    processed_data = process_heart_rate_data(items)
+    if not processed_data:
+        raise HTTPException(status_code=404, detail="유저의 데이터가 없음")
+    
+    df = pd.DataFrame(processed_data)
+    df['ds'] = pd.to_datetime(df['ds'])
+    
+    forecast = predict_heart_rate(df)
+    save_prediction_to_mongodb(user_email, forecast)
+    return {"message": "분석 끝 예측 데이터 저장", "데이터 길이": len(df)}
+
+  
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
