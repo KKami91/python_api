b3e75cb32f33f4347a8537cf62cd059efe40f66d
KKami91 <ghkdth919@gmail.com>
2024-07-05 11:54:41 +0900

    3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from pydantic import BaseModel
 import boto3
@@ -13,8 +14,18 @@ from typing import Optional
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+
 app = FastAPI()
 load_dotenv()
+
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://pnu-kkami2.vercel.app"],  # 프론트엔드 도메인
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 
 # AWS
