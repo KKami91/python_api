c0def0b9e485bacdff302b0bbeae3cdf81afda28
KKami91 <ghkdth919@gmail.com>
2024-07-10 15:15:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -274,6 +274,64 @@ def create_dataframe(json_data):
     
     return df
 
+def get_time_domain_features(nn_intervals) -> dict:
+    # nn_intervals(ms) => 60000/bpm
+
+    # nn intervals의 차이 (각 인접한 다음 nn interval의 차)
+    diff_nni = np.diff(nn_intervals)
+    # 데이터 길이
+    length_int = len(nn_intervals)
+
+    # Basic statistics
+    # 
+    mean_nni = np.mean(nn_intervals)
+    median_nni = np.median(nn_intervals)
+    range_nni = max(nn_intervals) - min(nn_intervals)
+
+    sdsd = np.std(diff_nni)
+    rmssd = np.sqrt(np.mean(diff_nni ** 2))
+
+    nni_50 = sum(np.abs(diff_nni) > 50)
+    pnni_50 = 100 * nni_50 / length_int
+    nni_20 = sum(np.abs(diff_nni) > 20)
+    pnni_20 = 100 * nni_20 / length_int
+    rmssd = np.sqrt(np.mean(np.square(np.diff(nn_intervals))))
+
+    # Feature found on github and not in documentation
+    cvsd = rmssd / mean_nni
+
+    # Features only for long term recordings
+    sdnn = np.std(nn_intervals, ddof=1)  # ddof = 1 : unbiased estimator => divide std by n-1
+    cvnni = sdnn / mean_nni
+
+    # Heart Rate equivalent features, heart_rate => bpm
+    heart_rate_list = np.divide(60000, nn_intervals)
+    mean_hr = np.mean(heart_rate_list)
+    min_hr = min(heart_rate_list)
+    max_hr = max(heart_rate_list)
+    std_hr = np.std(heart_rate_list)
+
+    time_domain_features = {
+        'mean_nni': mean_nni,
+        'rmssd': rmssd,
+        'sdnn': sdnn,
+        'sdsd': sdsd,
+        'nni_50': nni_50,
+        'pnni_50': pnni_50,
+        'nni_20': nni_20,
+        'pnni_20': pnni_20,
+        'rmssd': rmssd,
+        'median_nni': median_nni,
+        'range_nni': range_nni,
+        'cvsd': cvsd,
+        'cvnni': cvnni,
+        'mean_hr': mean_hr,
+        "max_hr": max_hr,
+        "min_hr": min_hr,
+        "std_hr": std_hr,
+    }
+
+    return time_domain_features
 
 def preprocess_analysis(df):
     # 1시간 단위로 데이터 변경
@@ -302,9 +360,6 @@ def preprocess_analysis(df):
     for value in dict_temp.values():
         value_list.append(value)
         
-    peaks_list = []
-    for i in range(len(value_list)):
-        peaks_list.append(nk.intervals_to_peaks(value_list[i]))
         
     res_dict = {}
     sdnn = []
@@ -312,10 +367,10 @@ def preprocess_analysis(df):
     #lf = []
     #hf = []
     
-    for i in range(len(peaks_list)):
-        temp_nk = nk.hrv(peaks_list[i])
-        sdnn.append(temp_nk['HRV_SDNN'][0])
-        rmssd.append(temp_nk['HRV_RMSSD'][0])
+    for i in range(len(value_list)):
+        get_domain_hrv = get_time_domain_features(value_list[i])
+        sdnn.append(np.round(get_domain_hrv['sdnn'],1))
+        rmssd.append(np.round(get_domain_hrv['rmssd'],1))
         #lf.append(temp_nk['HRV_LF'])
         #hf.append(temp_nk['HRV_HF'])
     
