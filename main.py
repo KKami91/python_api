79c5c0afd5f75dfa64701c7966f1f60e1404366a
KKami91 <ghkdth919@gmail.com>
2024-08-09 14:26:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ def create_bpm_dataframe_(bpm_data):
     future = model.make_future_dataframe(periods=len(pred_hour_df)-length_, freq='h')
     forecast = model.predict(future)
     
-    ds_df = pd.DataFrame({'ds': forecast['ds'][x] for x in range(len(forecast))})
+    ds_df = pd.DataFrame({'ds': [forecast['ds'][x] for x in range(len(forecast))]})
     new_hour_df = pd.merge(ds_df, hour_df[['ds','bpm','rmssd','sdnn']], on='ds', how='left')
     new_hour_df = pd.merge(new_hour_df, forecast[['yhat']], on='ds', how='left')
     
