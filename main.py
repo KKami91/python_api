8f03dc295d9f215794e6a11d0de37de33207925f
KKami91 <ghkdth919@gmail.com>
2024-10-07 14:38:05 +0900

    user_email_1_timestamp_1 batch 10000

--- main.py
+++ main.py
@@ -394,7 +394,7 @@ async def update_db(user_email, df, collection):
         ) for doc in documents
     ]
     
-    batch_size = 1000  # Reduced batch size for better performance
+    batch_size = 10000  # Reduced batch size for better performance
     total_operations = len(bulk_operations)
     total_updated = 0
     
