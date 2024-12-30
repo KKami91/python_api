ea415dcd9a697b7b06cfd69abcafbbb1537fa2ea
KKami91 <ghkdth919@gmail.com>
2024-12-30 18:15:18 +0900

    step분석, 수면 stage 평균 bpm 추가

--- main.py
+++ main.py
@@ -159,8 +159,8 @@ def configure_matplotlib():
         plt.rcParams['font.family'] = font_prop.get_name()
     
     plt.rcParams['axes.unicode_minus'] = False
-    plt.rcParams['figure.dpi'] = 300
-    plt.rcParams['savefig.dpi'] = 300
+    # plt.rcParams['figure.dpi'] = 300
+    # plt.rcParams['savefig.dpi'] = 300
 
 async def user_info_update():
     results = [dynamodb.scan(IndexName='email-timestamp-index', TableName=TABLE_NAME)][0]['Items']
@@ -182,35 +182,129 @@ async def user_info_update():
             upsert=True
         )
         
-async def get_bpm_df(user_email: str):
-    user_bpm_data = await bpm.find({'user_email': user_email}).to_list(length=None)
-    bpm_df = pd.DataFrame({
-        'timestamp': [user_bpm_data[x]['timestamp'] for x in range(len(user_bpm_data))],
-        'bpm': [user_bpm_data[x]['value'] for x in range(len(user_bpm_data))]
-    })  
-    bpm_df['timestamp'] = pd.to_datetime(bpm_df['timestamp']).dt.tz_localize('UTC').dt.tz_convert('Asia/Seoul').dt.tz_localize(None)  
-    return bpm_df
-        
-######## 수면 분석 함수 ###########
-def analyze_sleep_patterns(user_sleep_dict):
-    # 각 사용자별 수면 단계 분포
-    sleep_stage_distribution = {}
-    for user_email, sleep_df in user_sleep_dict.items():
-        # 각 수면 단계별 시간 계산
-        stage_durations = sleep_df.groupby('stage').apply(
-            lambda x: ((x['timestamp_end'] - x['timestamp_start']).dt.total_seconds()).sum()
-        ) / 3600  # 시간 단위로 변환
-        sleep_stage_distribution[user_email] = stage_durations
+######## 걸음수 분석 #########
+def step_weekday_average(df):
+    result = {}
+    print('in step def :', df)
+    for weekday, weekday_group in df.groupby('weekdays'):
+        Q1 = np.quantile(weekday_group['step'], 0.25)
+        Q3 = np.quantile(weekday_group['step'], 0.75)
+        IQR = Q3 - Q1
+        upper_bound = Q3 + 1.5 * IQR
+
+        cleaned_data = weekday_group[weekday_group['step'] <= upper_bound]
+        unique_dates = cleaned_data['timestamp'].dt.normalize().nunique()
+        result[weekday] = np.round(cleaned_data['step'].sum() / unique_dates, 1)
+    return result
+
+def step_hour_average(df):
+    result = {}
+    for hour, hour_group in df.groupby('hours'):
+        Q1 = np.quantile(hour_group['step'], 0.25)
+        Q3 = np.quantile(hour_group['step'], 0.75)
+        IQR = Q3 - Q1
+        upper_bound = Q3 + 1.5 * IQR
+
+        cleaned_data = hour_group[hour_group['step'] <= upper_bound]
+        unique_dates = cleaned_data['timestamp'].dt.normalize().nunique()
+        result[hour] = np.round(cleaned_data['step'].sum() / unique_dates, 1)
+    return result
+
+@app.post("/user_analysis_step/{user_email}")
+async def plot_user_analysis_step(user_email: str):
+    user_info_data = await user_info.find_one({'user_email': user_email})
+    user_name = user_info_data['user_name']
+    user_height = user_info_data['user_height']
+    user_weight = user_info_data['user_weight']
+    user_bmi = np.round(int(user_weight)/((int(user_height) / 100) * 2), 3)
+    user_gender = user_info_data['user_gender'][:1]
+    user_birth = datetime.strptime(user_info_data['user_birth'], "%Y-%m-%d")
+    today = datetime.now()
+    user_age = today.year - user_birth.year
+    if (today.month, today.day) < (user_birth.month, user_birth.day):
+        user_age -= 1
+    user_step_data = await step.find({'user_email': user_email}).to_list(length=None)
+    step_df = pd.DataFrame({
+        'timestamp': [user_step_data[x]['timestamp'] for x in range(len(user_step_data))],
+        'step': [user_step_data[x]['value'] for x in range(len(user_step_data))]
+    })
+    step_df['timestamp'] = pd.to_datetime(step_df['timestamp']).dt.tz_localize('UTC').dt.tz_convert('Asia/Seoul').dt.tz_localize(None)
+    step_df['hours'] = step_df['timestamp'].dt.hour
     
-    data_quality = {}
-    for user_email, sleep_df in user_sleep_dict.items():
-        # 실제 수면 기록이 있는 날짜 수
-        recorded_days = sleep_df['timestamp_start'].dt.date.nunique()
-        data_quality[user_email] = {
-            'recorded_days': recorded_days,
-        }
+    week_dict = {0:'월', 1:'화', 2:'수', 3:'목', 4:'금', 5:'토', 6:'일'}
+    step_df['weekdays'] = step_df['timestamp'].dt.weekday.map(week_dict)
+    hourly_average = step_hour_average(step_df)
+    weekday_average = step_weekday_average(step_df)
+    print('-----weekday_average-----')
+    print(weekday_average)
+    
+    
+    sorted_weekday = ['월', '화', '수', '목', '금', '토', '일']
+    # conv_weekday_average = {day: weekday_average[day] for day in sorted_weekday if day in weekday_average}
+    sorted_weekday_averages = {day: weekday_average[day] for day in sorted_weekday if day in weekday_average}
+    
+    hours = hourly_average.keys()
+    hours_steps = hourly_average.values()
+    
+    print('before weekdays, weekdays_steps')
+    print(sorted_weekday_averages)
+    weekdays = sorted_weekday_averages.keys()
+    weekdays_steps = sorted_weekday_averages.values()
+    print('after weekdays, weekdays_steps')
+    print(weekdays, weekdays_steps)
+    
+    try:
+        configure_matplotlib()
+        # fig = plt.figure(figsize=(20, 16), constrained_layout=True)
+        fig = plt.figure(figsize=(20, 16))
+        # gs = fig.add_gridspec(2, 1, height_ratios=[1, 1.2], hspace=0.1)
+        gs = fig.add_gridspec(2, 1, height_ratios=[1, 1.2])
+        plt.rc('figure', titlesize=15)
+        plt.gcf().subplots_adjust(top=0.85, left=0.2, bottom=0.1, right=0.85, hspace=0.4)
         
-    return sleep_stage_distribution, data_quality
+        #### 시간별 ####
+        ax1 = fig.add_subplot(gs[0])
+        bar1 = ax1.bar(hours, hours_steps)
+        for rect in bar1:
+            height = rect.get_height()
+            ax1.text(rect.get_x() + rect.get_width()/2.0, height, '%.1f' % height, ha='center', va='bottom', size=12)
+        ax1.set_title(f'시간별 평균 걸음 수 \n {user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi)',
+                        fontsize=20, 
+                        pad=20,
+                        linespacing=1.5)
+        ax1.set_ylabel('걸음 수', fontsize=14)
+        ax1.set_xlabel('시간', fontsize=14)
+        ax1.set_xticks(ticks=range(0,24))
+        
+        #### 요일별 ####
+        ax2 = fig.add_subplot(gs[1])
+        print(weekdays, weekdays_steps)
+        bar2 = ax2.bar(weekdays, weekdays_steps)
+        for rect in bar2:
+            height = rect.get_height()
+            ax2.text(rect.get_x() + rect.get_width()/2.0, height, '%.1f' % height, ha='center', va='bottom', size=12)
+        ax2.set_title(f'일별 평균 걸음수',
+                      fontsize=20,
+                      pad=20,
+                      linespacing=1.5)
+        ax2.set_ylabel('걸음 수', fontsize=14)
+        ax2.set_xlabel('요일', fontsize=14)
+        
+        
+    
+        # 이미지 PNG로 변환
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png', pad_inches=0.5, dpi=100)
+        buf.seek(0)
+        plt.close()
+        
+        return StreamingResponse(buf, media_type="image/png")
+    except Exception as e:
+        print(f"error generating plot : {str(e)}")
+        raise HTTPException(status_code=500, detail="error generating plot")
+    finally:
+        plt.close('all')
+        gc.collect()
 
 ######## 수면 stage별 평균 심박수 ######
 def analyze_sleep_heart_rate(user_sleep_dict, user_bpm_dict):
@@ -252,6 +346,27 @@ def analyze_sleep_heart_rate(user_sleep_dict, user_bpm_dict):
         }
     
     return sleep_hr_analysis
+        
+######## 수면 분석 함수 ###########
+def analyze_sleep_patterns(user_sleep_dict):
+    # 각 사용자별 수면 단계 분포
+    sleep_stage_distribution = {}
+    for user_email, sleep_df in user_sleep_dict.items():
+        # 각 수면 단계별 시간 계산
+        stage_durations = sleep_df.groupby('stage').apply(
+            lambda x: ((x['timestamp_end'] - x['timestamp_start']).dt.total_seconds()).sum()
+        ) / 3600  # 시간 단위로 변환
+        sleep_stage_distribution[user_email] = stage_durations
+    
+    data_quality = {}
+    for user_email, sleep_df in user_sleep_dict.items():
+        # 실제 수면 기록이 있는 날짜 수
+        recorded_days = sleep_df['timestamp_start'].dt.date.nunique()
+        data_quality[user_email] = {
+            'recorded_days': recorded_days,
+        }
+        
+    return sleep_stage_distribution, data_quality
 
 def int_to_str_time(int_time):
     hours = int(int_time)
@@ -262,7 +377,9 @@ def int_to_str_time(int_time):
 
 @app.post("/user_analysis_sleep/{user_email}")
 async def plot_user_analysis_sleep(user_email: str):
+    print('---------1---------')
     user_info_data = await user_info.find_one({'user_email': user_email})
+    print('---------2--------')
     user_name = user_info_data['user_name']
     user_height = user_info_data['user_height']
     user_weight = user_info_data['user_weight']
@@ -273,6 +390,7 @@ async def plot_user_analysis_sleep(user_email: str):
     user_age = today.year - user_birth.year
     if (today.month, today.day) < (user_birth.month, user_birth.day):
         user_age -= 1
+    print('---------3---------')
     # 수면 전용
     user_sleep_data = await sleep.find({'user_email': user_email}).to_list(length=None)
     sleep_df = pd.DataFrame({
@@ -288,8 +406,12 @@ async def plot_user_analysis_sleep(user_email: str):
     sleep_bpm_analysis = analyze_sleep_heart_rate({user_email: sleep_df}, {user_email: bpm_df})
     sleep_bpm_stat = sleep_bpm_analysis[user_email]['stage_analysis']
     # total_sleep_time = sum(item['total_duration'] for item in sleep_bpm_stat.values())
+    print(sleep_bpm_stat, '@@@@@@@@@@@')
+    print(sleep_bpm_stat.items(), '!!!!!!')
+    
     
     try:
+        configure_matplotlib()
         stage_dict = {1: 'Awake', 4: 'Light', 5: 'Deep', 6: 'Rem'}
         mean_hr = [item['mean_hr'] for item in sleep_bpm_stat.values()]
         stages = [stage_dict[item] for item in sleep_bpm_stat.keys()]
@@ -305,17 +427,17 @@ async def plot_user_analysis_sleep(user_email: str):
                 autopct='%1.1f%%',
                 startangle=90,
                 textprops={'ha':'center', 'va':'center'},
-                labeldistance=1.28)
-        plt.title(f"수면 분석\n {user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi) \n 총 수면 시간 : {int_to_str_time(total_hours)}, 전체 기록 기간 : {sleep_data_analysis[1][user_email]['recorded_days']}일",
+                labeldistance=1.3)
+        plt.title(f"{user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi) \n\n 수면 분석\n 총 수면 시간 : {int_to_str_time(total_hours)}, 전체 기록 기간 : {sleep_data_analysis[1][user_email]['recorded_days']}일",
                   fontsize=14,
                   loc='center',
-                  pad=20,
+                  pad=40,
                   linespacing=1.5)
-        plt.gcf().subplots_adjust(top=0.85)
+        plt.gcf().subplots_adjust(top=0.75)
         
         plt.legend(stages, title="Sleep Stages", loc=(1,0.8))
         buf = io.BytesIO()
-        plt.savefig(buf, format='png', pad_inches=0.5)
+        plt.savefig(buf, format='png', pad_inches=0.5, dpi=180)
         buf.seek(0)
         plt.close()
         
@@ -326,8 +448,47 @@ async def plot_user_analysis_sleep(user_email: str):
     finally:
         plt.close('all')
         gc.collect()
+    
+    # try:
+    #     for user_email, stages in sleep_data_analysis[0].items():
+    #         # 총 시간 계산
+    #         total_hours = stages.sum()
+    #         # 비율 계산
+    #         stage_percentages = (stages / total_hours) * 100
+    #         stage_dict = {1: 'Awake', 4: 'Light', 5: 'Deep', 6: 'Rem'}
+            
+    #         # 파이 차트 생성
+            
+    #         plt.pie(stage_percentages, 
+    #                 labels=[f'{stage_dict[stage]}' for stage, percentage in stage_percentages.items()],
+    #                 autopct='%1.1f%% \n{}',
+    #                 startangle=90)
+    #         #plt.title(f"수면 분석 - {user_email} \n {user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi) \n 총 수면 시간 : {np.round(total_hours,2)}시간, 전체 기록 기간 : {sleep_data_analysis[1][user_email]['recorded_days']}일", fontsize=8)
+    #         plt.title(f"수면 분석 - {user_email} \n {user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi) \n 총 수면 시간 : {int_to_str_time(total_hours)}, 전체 기록 기간 : {sleep_data_analysis[1][user_email]['recorded_days']}일", fontsize=8)
+    #             # 이미지 PNG로 변환
+    #     plt.legend(loc=(1,0.6))
+    #     buf = io.BytesIO()
+    #     plt.savefig(buf, format='png', pad_inches=0.5)
+    #     buf.seek(0)
+    #     plt.close()
+        
+    #     return StreamingResponse(buf, media_type="image/png")
+    # except Exception as e:
+    #     print(f"error generating plot : {str(e)}")
+    #     raise HTTPException(status_code=500, detail="error generating plot")
+    # finally:
+    #     plt.close('all')
+    #     gc.collect()
         
         
+async def get_bpm_df(user_email: str):
+    user_bpm_data = await bpm.find({'user_email': user_email}).to_list(length=None)
+    bpm_df = pd.DataFrame({
+        'timestamp': [user_bpm_data[x]['timestamp'] for x in range(len(user_bpm_data))],
+        'bpm': [user_bpm_data[x]['value'] for x in range(len(user_bpm_data))]
+    })  
+    bpm_df['timestamp'] = pd.to_datetime(bpm_df['timestamp']).dt.tz_localize('UTC').dt.tz_convert('Asia/Seoul').dt.tz_localize(None)  
+    return bpm_df
 
 @app.post("/user_analysis_bpm/{user_email}")
 async def plot_user_analysis_bpm(user_email: str):
@@ -343,16 +504,18 @@ async def plot_user_analysis_bpm(user_email: str):
     if (today.month, today.day) < (user_birth.month, user_birth.day):
         user_age -= 1
     
-    user_bpm_data = await bpm.find({'user_email': user_email}).to_list(length=None)
-    #user_hour_rmssd_data = await hour_rmssd.find({'user_email': user_email})
-    #user_hour_sdnn_data = await hour_sdnn.find({'user_email': user_email})
+    # user_bpm_data = await bpm.find({'user_email': user_email}).to_list(length=None)
+    # #user_hour_rmssd_data = await hour_rmssd.find({'user_email': user_email})
+    # #user_hour_sdnn_data = await hour_sdnn.find({'user_email': user_email})
     
-    bpm_df = pd.DataFrame({
-        'timestamp': [user_bpm_data[x]['timestamp'] for x in range(len(user_bpm_data))],
-        'bpm': [user_bpm_data[x]['value'] for x in range(len(user_bpm_data))]
-    })
+    # bpm_df = pd.DataFrame({
+    #     'timestamp': [user_bpm_data[x]['timestamp'] for x in range(len(user_bpm_data))],
+    #     'bpm': [user_bpm_data[x]['value'] for x in range(len(user_bpm_data))]
+    # })
     
-    bpm_df['timestamp'] = pd.to_datetime(bpm_df['timestamp']).dt.tz_localize('UTC').dt.tz_convert('Asia/Seoul').dt.tz_localize(None)
+    # bpm_df['timestamp'] = pd.to_datetime(bpm_df['timestamp']).dt.tz_localize('UTC').dt.tz_convert('Asia/Seoul').dt.tz_localize(None)
+    
+    bpm_df = await get_bpm_df(user_email)
     
     # 시간대별 평균 BPM plot 전용
     bpm_df['hour_float'] = bpm_df['timestamp'].dt.hour + bpm_df['timestamp'].dt.minute / 60.0
@@ -373,12 +536,12 @@ async def plot_user_analysis_bpm(user_email: str):
     try:
         # 배포 전용 함수
         configure_matplotlib()
-        # font_path = '/usr/share/fonts/truetype/nanum/NanumGothic.ttf'
-        # font_prop = fm.FontProperties(fname=font_path)
         fig = plt.figure(figsize=(20, 25), constrained_layout=True)
-        gs = fig.add_gridspec(2, 1, height_ratios=[1, 1.2], hspace=0.1)
+        # fig = plt.figure(figsize=(16, 25))
+        # gs = fig.add_gridspec(2, 1, height_ratios=[1, 1.2], hspace=0.1)
+        gs = fig.add_gridspec(3, 1, height_ratios=[1, 0.05, 1.2])
         plt.rc('figure', titlesize=15)
-        plt.gcf().subplots_adjust(top=0.85)
+        plt.gcf().subplots_adjust(top=0.85, left=0.2, bottom=0.1, right=0.85, hspace=0.4)
         
         
         # 시간대별 평균 BPM plot
@@ -387,8 +550,8 @@ async def plot_user_analysis_bpm(user_email: str):
         ax1.fill_between(range(len(hourly_bpm)), hourly_bpm['mean'] - hourly_bpm['std'], hourly_bpm['mean'] + hourly_bpm['std'], alpha=0.2)
         ax1.set_xticks(range(len(hourly_bpm)))
         ax1.set_xticklabels(hourly_bpm['time_bin'], rotation=45)
-        ax1.set_title(f'시간대 평균 심박수 \n {user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi)',
-                      fontsize=20,
+        ax1.set_title(f'{user_email}, {user_name}({user_gender}), {user_age}세, {user_height}cm, {user_weight}kg, {user_bmi}(kg/m^2 = bmi)\n\n시간대 평균 심박수',
+                      fontsize=20, 
                       pad=20,
                       linespacing=1.5)
         ax1.set_ylabel('심박수', fontsize=14)
@@ -398,17 +561,20 @@ async def plot_user_analysis_bpm(user_email: str):
         
         # 날짜와 시간별 평균 BPM Heatmap
         pivot_table = bpm_df.pivot_table(values='bpm', index='date', columns='hour', aggfunc='mean')
-        ax2 = fig.add_subplot(gs[1])
-        sns.heatmap(pivot_table, cmap='YlOrRd', xticklabels=True, yticklabels=True, vmin=vmin+10, vmax=vmax-10, ax=ax2)
-        ax2.set_title(f'날짜 - 시간대 평균 심박수 히트맵', 
-                      fontsize=20, 
-                      pad=20)
+        ax2 = fig.add_subplot(gs[2])
+        # heatmap = sns.heatmap(pivot_table, cmap='YlOrRd', xticklabels=True, yticklabels=True, vmin=vmin+10, vmax=vmax-10, ax=ax2, cbar_kws={'use_gridspec': True})
+        sns.heatmap(pivot_table, cmap='YlOrRd', xticklabels=True, yticklabels=True, vmin=vmin+10, vmax=vmax-10, ax=ax2, cbar_kws={'use_gridspec': True})
+        plt.gcf().subplots_adjust(top=0.85, left=0.2, bottom=0.1, right=0.85, hspace=0.4)
+        #cbar = heatmap.collections[0].colorbar
+        #cbar.ax.set_position([0.87, 0.1, 0.03, 0.3])
+        ax2.set_title(f'날짜 - 시간대 평균 심박수 히트맵', fontsize=20, pad=20)
         ax2.set_xlabel('시간', fontsize=14)
         ax2.set_ylabel('날짜', fontsize=14)
-
+        
+        
         # 이미지 PNG로 변환
         buf = io.BytesIO()
-        plt.savefig(buf, format='png', pad_inches=0.5)
+        plt.savefig(buf, format='png', pad_inches=0.5, dpi=100)
         buf.seek(0)
         plt.close()
         
