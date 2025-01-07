d25858bcb32835dd3e0bbef243d97f36cedecd9c
KKami91 <ghkdth919@gmail.com>
2025-01-07 11:03:46 +0900

    CORS Error -> origin *

--- main.py
+++ main.py
@@ -44,7 +44,8 @@ class UserEmailRequest(BaseModel):
 # CORS 미들웨어 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pnu-kkami2.vercel.app"],  # 프론트엔드 도메인
+    #allow_origins=["https://pnu-kkami2.vercel.app"],  # 프론트엔드 도메인
+    allow_origins=["*"],  # 프론트엔드 도메인
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
